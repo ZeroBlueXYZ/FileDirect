81d72d01c600b6b6e821dcdacc8c540219904aa9
zeroblue <zerobluexyz@gmail.com>
2023-10-23 21:00:14 -0700

    Override build name and build number in windows build script

--- scripts/build_windows.bat
+++ scripts/build_windows.bat
@@ -1,13 +1,15 @@
 echo off
 
-IF "%~1" == "" goto :error
-set VERSION=%1
+if "%~1" == "" goto :error
+if "%~2" == "" goto :error
+set BUILD_NAME=%1
+set BUILD_NUMBER=%2
 
-set DEBUG_INFO_DIR=.\debug_info\%VERSION%\windows
+set DEBUG_INFO_DIR=.\debug_info\%BUILD_NAME%+%BUILD_NUMBER%\windows
 
 if not exist %DEBUG_INFO_DIR% mkdir %DEBUG_INFO_DIR%
-flutter build windows --obfuscate --split-debug-info=%DEBUG_INFO_DIR%
+flutter build windows --obfuscate --split-debug-info=%DEBUG_INFO_DIR% --build-name=%BUILD_NAME% --build-number=%BUILD_NUMBER%
 goto :eof
 
 :error
-echo "Version cannot be empty"
+echo "Both build name and build number must be specified"
