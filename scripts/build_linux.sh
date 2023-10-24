84ee3c71e9efa30196e15ea343a5587dbb063488
zmzhang <8111354+zmzhang8@users.noreply.github.com>
2023-10-23 20:39:20 -0700

    Override build name and build number in build scripts

--- scripts/build_linux.sh
+++ scripts/build_linux.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 set -e
 
-VERSION="$1"
-if ! [ -n "$VERSION" ]; then
-    echo "Version cannot be empty"
+BUILD_NAME="$1"
+BUILD_NUMBER="$2"
+if ! { [ -n "$BUILD_NAME" ] && [ -n "$BUILD_NUMBER" ]; }; then
+    echo "Both build name and build number must be specified"
     exit 1
 fi
 
-DEBUG_INFO_DIR=./debug_info/$VERSION/linux
+DEBUG_INFO_DIR=./debug_info/$BUILD_NAME+$BUILD_NUMBER/linux
 BUNDLE_DIR=./build/linux/x64/release/bundle/
 
 mkdir -p $DEBUG_INFO_DIR
-flutter build linux --obfuscate --split-debug-info=$DEBUG_INFO_DIR
+flutter build linux \
+    --obfuscate \
+    --split-debug-info=$DEBUG_INFO_DIR \
+    --build-name=$BUILD_NAME \
+    --build-number=$BUILD_NUMBER
 pushd $BUNDLE_DIR
 zip -r "../anysend-$VERSION.zip" *
 popd
--- scripts/build_macos.sh
+++ scripts/build_macos.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 set -e
 
-VERSION="$1"
-if ! [ -n "$VERSION" ]; then
-    echo "Version cannot be empty"
+BUILD_NAME="$1"
+BUILD_NUMBER="$2"
+if ! { [ -n "$BUILD_NAME" ] && [ -n "$BUILD_NUMBER" ]; }; then
+    echo "Both build name and build number must be specified"
     exit 1
 fi
 
-DEBUG_INFO_DIR=./debug_info/$VERSION/macos
+DEBUG_INFO_DIR=./debug_info/$BUILD_NAME+$BUILD_NUMBER/macos
 
 mkdir -p $DEBUG_INFO_DIR
-flutter build macos --obfuscate --split-debug-info=$DEBUG_INFO_DIR
+flutter build macos \
+    --obfuscate \
+    --split-debug-info=$DEBUG_INFO_DIR \
+    --build-name=$BUILD_NAME \
+    --build-number=$BUILD_NUMBER
