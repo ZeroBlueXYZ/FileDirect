f3cc64db8597c5b43fad4b90aa77761c8d949b90
zmzhang <8111354+zmzhang8@users.noreply.github.com>
2023-10-24 21:08:05 -0700

    Fix zip file name for linux build script

--- scripts/build_linux.sh
+++ scripts/build_linux.sh
@@ -18,5 +18,5 @@ flutter build linux \
     --build-name=$BUILD_NAME \
     --build-number=$BUILD_NUMBER
 pushd $BUNDLE_DIR
-zip -r "../anysend-$VERSION.zip" *
+zip -r "../anysend-$BUILD_NAME+$BUILD_NUMBER.zip" *
 popd
