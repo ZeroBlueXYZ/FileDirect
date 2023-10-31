5aa0cec2bc80704f39128d4295c41134c15ccfaf
zmzhang <8111354+zmzhang8@users.noreply.github.com>
2023-10-30 23:18:28 -0700

    Update linux build script

--- scripts/build_linux.sh
+++ scripts/build_linux.sh
@@ -9,7 +9,8 @@ if ! { [ -n "$BUILD_NAME" ] && [ -n "$BUILD_NUMBER" ]; }; then
 fi
 
 DEBUG_INFO_DIR=./debug_info/$BUILD_NAME+$BUILD_NUMBER/linux
-BUNDLE_DIR=./build/linux/x64/release/bundle/
+BUILD_DIR=./build
+BUNDLE_DIR=./linux/x64/release/bundle
 
 mkdir -p $DEBUG_INFO_DIR
 flutter build linux \
@@ -17,6 +18,8 @@ flutter build linux \
     --split-debug-info=$DEBUG_INFO_DIR \
     --build-name=$BUILD_NAME \
     --build-number=$BUILD_NUMBER
-pushd $BUNDLE_DIR
-zip -r "../anysend-$BUILD_NAME.zip" *
+pushd $BUILD_DIR
+cp -r $BUNDLE_DIR anysend
+tar -czvf "anysend-$BUILD_NAME.tar.gz" anysend
+rm -rf anysend
 popd
